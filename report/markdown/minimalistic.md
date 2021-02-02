acf28a242b72eb13b806eb3ef2c0f474b9a19c23
taylor-a-barnes <taylor.a.barnes@gmail.com>
2021-02-02 17:21:07 -0500

    Update report

--- report/markdown/minimalistic.md
+++ report/markdown/minimalistic.md
@@ -2,27 +2,17 @@
 
 This test verifies that the engine is capable of basic communication with an external engine.
 
+
+
 ## Failure Resolution
 
 If this test fails, ensure that you have followed each of the steps outlined below.
 
-### Step 1: Make the MDI Library Available to the Engine
 
-In order to pass this test, the engine must be capable of calling the functions that are defined by the MDI Library, such as `MDI_Init()`.
-The process for enabling this is fundamentally different depending on whether the engine is written in a compiled or interpreted language.
 
-**Compiled Languages:** In the case of codes written in compiled languages, this requires that the code be compiled and linked against the MDI Library.
 
-**Interpreted Languages:** The only interpreted language currently supported by MDI is Python.
-If your engine is written in Python, you should first install the MDI Library during the `build_image` step in `mdimechanic.yml`.
-This can be done trivially using `pip` (*e.g.* `pip install pymdi`).
-Your engine can then import the MDI Library where needed (*e.g.* `import mdi`).
+### Step 7: Add Support for Additional Nodes
 
-### Step 2: Initialize the MDI Library
-
-Your code must initialize the MDI Library by calling the `MDI_Init()` function.
-If your code uses the Message Passing Interface (MPI), you should call `MDI_Init()` after the call to `MPI_Init()`.
-Otherwise, `MDI_Init()` should be called as early your code as possible.
-
-### Step 3: Support Basic MDI Communication
-
+Whenever you add a new node, you must also add a call to `MDI_Register_Node()` to register support for that node.
+Commands are registered separately for each node, so any commands that are supported at the new node must be registered for it.
+For example, if you implement five nodes, and each of them supports the `EXIT` command, you will need to call the `MDI_Register_Command()` function five times, each time with a different node as the first argument and with `EXIT` as the second argument.
---
+++ tests/drivers/mytest/mytest.py
@@ -0,0 +1,104 @@
+import mdi
+
+# Attempt to import mpi4py
+try:
+    from mpi4py import MPI
+    use_mpi4py = True
+except ImportError:
+    use_mpi4py = False
+ 
+# MPI intra-communicator for all processes running this code
+# It should be set to MPI.COMM_WORLD prior to the call to MDI_Init(), as shown below
+# Afterwards, you should ALWAYS use this variable instead of MPI.COMM_WORLD
+if use_mpi4py:
+    world_comm = MPI.COMM_WORLD
+else:
+    world_comm = None
+
+# Get the command-line options for MDI
+
+# Iniitalize MDI
+mdi_options = "-role DRIVER -name driver -method TCP -port 8021"
+mdi.MDI_Init(mdi_options, world_comm)
+
+# Set world_comm to the correct intra-code MPI communicator
+world_comm = mdi.MDI_Get_Intra_Code_MPI_Comm()
+
+# Get the MPI rank of this process
+if world_comm is not None:
+    my_rank = world_comm.Get_rank()
+else:
+    my_rank = 0
+ 
+# Accept a connection from an external driver
+mdi_comm = mdi.MDI_Accept_Communicator()
+
+# This is the part where you can use MDI
+if my_rank == 0:
+    # <NAME
+    mdi.MDI_Send_Command("<NAME", mdi_comm)
+    engine_name = mdi.MDI_Recv(mdi.MDI_NAME_LENGTH, mdi.MDI_CHAR, mdi_comm)
+    print("ENGINE NAME: " + str(engine_name))
+
+    # <ENERGY
+    mdi.MDI_Send_Command("<ENERGY", mdi_comm)
+    energy = mdi.MDI_Recv(1, mdi.MDI_DOUBLE, mdi_comm)
+    print("ENERGY:  " + str(energy))
+
+    # <KE
+    mdi.MDI_Send_Command("<KE", mdi_comm)
+    energy = mdi.MDI_Recv(1, mdi.MDI_DOUBLE, mdi_comm)
+    print("KE:       " + str(energy))
+
+    # <KE_ELEC
+    mdi.MDI_Send_Command("<KE_ELEC", mdi_comm)
+    energy = mdi.MDI_Recv(1, mdi.MDI_DOUBLE, mdi_comm)
+    print("KE_ELEC:  " + str(energy))
+
+    # <PE
+    mdi.MDI_Send_Command("<PE", mdi_comm)
+    energy = mdi.MDI_Recv(1, mdi.MDI_DOUBLE, mdi_comm)
+    print("PE:      " + str(energy))
+
+    # <PE_ELEC
+    mdi.MDI_Send_Command("<PE_ELEC", mdi_comm)
+    energy = mdi.MDI_Recv(1, mdi.MDI_DOUBLE, mdi_comm)
+    print("PE_ELEC: " + str(energy))
+
+    # <DIMENSIONS
+    mdi.MDI_Send_Command("<DIMENSIONS", mdi_comm)
+    dimensions = mdi.MDI_Recv(3, mdi.MDI_INT, mdi_comm)
+    print("DIMENSIONS: " + str(dimensions))
+
+    # <NATOMS
+    mdi.MDI_Send_Command("<NATOMS", mdi_comm)
+    natoms = mdi.MDI_Recv(1, mdi.MDI_INT, mdi_comm)
+    print("NATOMS: " + str(natoms))
+
+    # <COORDS
+    mdi.MDI_Send_Command("<COORDS", mdi_comm)
+    coords = mdi.MDI_Recv(3 * natoms, mdi.MDI_DOUBLE, mdi_comm)
+    print("COORDS: " + str(coords))
+
+    # >COORDS
+    coords[0] += 0.1
+    mdi.MDI_Send_Command(">COORDS", mdi_comm)
+    mdi.MDI_Send(coords, 3 * natoms, mdi.MDI_DOUBLE, mdi_comm)
+    mdi.MDI_Send_Command("<ENERGY", mdi_comm)
+    energy = mdi.MDI_Recv(1, mdi.MDI_DOUBLE, mdi_comm)
+    print("ENERGY:  " + str(energy))
+
+    # <CHARGES
+    mdi.MDI_Send_Command("<CHARGES", mdi_comm)
+    charges = mdi.MDI_Recv(natoms, mdi.MDI_DOUBLE, mdi_comm)
+    print("CHARGES: " + str(charges))
+
+    # <ELEMENTS
+    mdi.MDI_Send_Command("<ELEMENTS", mdi_comm)
+    elements = mdi.MDI_Recv(natoms, mdi.MDI_INT, mdi_comm)
+    print("ELEMENTS: " + str(elements))
+
+    # <FORCES
+
+    # EXIT
+    mdi.MDI_Send_Command("EXIT", mdi_comm)
